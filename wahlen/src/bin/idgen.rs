869b8196b4044892ebbb57189c8a90cbdaf56176
Ceri Storey <cez@necrofish.org.uk>
2019-09-14 09:12:39 +0100

    Use structopt to select number of items to generate.

--- wahlen/src/bin/idgen.rs
+++ wahlen/src/bin/idgen.rs
@@ -1,11 +1,22 @@
 use failure::Fallible;
 
 use infra::ids::IdGen;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "idgen", about = "Generate Identifiers")]
+struct Opt {
+    #[structopt(short = "n", long = "count", default_value = "1")]
+    count: usize,
+}
 
 fn main() -> Fallible<()> {
+    let opt = Opt::from_args();
+
     let idgen = IdGen::new();
-    let id = idgen.untyped();
-    println!("{}", id);
+    for _ in 0..opt.count {
+        println!("{}", idgen.untyped());
+    }
 
     Ok(())
 }
